489c16b1e24ac2e485549622c441e2bf85138624
stkwi <stkwiat22a@gmail.com>
2024-12-14 05:13:43 +0100

    Robiota221

--- app.py
+++ app.py
@@ -1,21 +1,28 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 from zapv2 import ZAPv2
 import requests
 import time
 import json
-from collections import defaultdict
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="templates", static_folder="static")
 
 # Konfiguracja połączenia z ZAP
 zap = ZAPv2(proxies={"http": "http://127.0.0.1:7071", "https": "http://127.0.0.1:7071"})
 
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
 @app.route('/analyze', methods=['POST'])
 def analyze():
+    print("Dzialamy")
     data = request.get_json()
     openapi_url = data.get('openapi_url')
     base_url = data.get('base_url')
-
+    print(openapi_url)
+    print(base_url)
     if not openapi_url or not base_url:
         return jsonify({"error": "Both 'openapi_url' and 'base_url' are required."}), 400
 
@@ -31,21 +38,16 @@ def analyze():
 
         # Uruchomienie skanowania
         zap.ascan.scan(url=base_url)
-        time.sleep(100)  # Poczekaj na zakończenie skanowania
+        time.sleep(10)  # Poczekaj na zakończenie skanowania
 
         # Pobranie alertów
         alerts = zap.core.alerts()
 
-        # Grupowanie alertów według pola 'risk'
-        grouped_alerts = defaultdict(list)
-        for alert in alerts:
-            risk_level = alert.get('risk', 'Unknown')
-            grouped_alerts[risk_level].append(alert)
-
-        return jsonify({"grouped_alerts": dict(grouped_alerts)})
+        return jsonify({"alerts": alerts})
 
     except Exception as e:
         return jsonify({"error": str(e)}), 500
+
 
 @app.route('/send-to-llama', methods=['POST'])
 def send_to_llama():
@@ -72,6 +74,7 @@ def send_to_llama():
 
     except Exception as e:
         return jsonify({"error": str(e)}), 500
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
